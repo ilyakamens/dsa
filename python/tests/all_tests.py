034f3ab88367002902c4899a3fa5c9fd093f9177
Ilya Kamens <ilyakamens@gmail.com>
2014-09-08 01:13:21 -0700

    Test script to run all tests

---
+++ python/tests/all_tests.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+__author__ = 'ilyakamens'
+__date__ = '9/8/14'
+
+from sieve_of_eratosthenes_test import *
+from prime_factorization_test import *
+
+class AllTestsTestCase(unittest.TestCase):
+    """Test class to run all tests"""
+
+    def test_all(self):
+        """Load all the test suites so they can be run"""
+        suite = unittest.TestSuite()
+        suite.addTests(unittest.TestLoader().loadTestsFromTestCase(SieveOfEratosthenesTestCase))
+        suite.addTests(unittest.TestLoader().loadTestsFromTestCase(PrimeFactorizationTestCase))
+
+if __name__ == '__main__':
+    unittest.main()
