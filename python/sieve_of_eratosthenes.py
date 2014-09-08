ea8458f5162049a2d4c389cf94d3d8f0666b5952
Ilya Kamens <ilyakamens@gmail.com>
2014-09-07 18:30:18 -0700

    Bug fixes and added functionality

--- python/sieve_of_eratosthenes.py
+++ python/sieve_of_eratosthenes.py
@@ -2,6 +2,7 @@
 __author__ = 'ilyakamens'
 __date__ = '9/6/14'
 
+
 def get_sieve(n):
     """Uses Eratosthenes's algorithm to determine whether each number
     from 0 - n is prime.
@@ -18,14 +19,30 @@ def get_sieve(n):
         nums.append(True)
 
     # for i = 2 ... √n
-    for i in xrange(2, int(n**.5)):
+    for i in xrange(2, int(n**0.5) + 1):
         if nums[i]:
             # for j = i^2, i^2 + i, i^2 + 2i, i^2 + 3i...
-            for j in xrange(i ** 2, n, i):
+            for j in xrange(i**2, n + 1, i):
                 nums[j] = False
 
     return nums
 
+
+def get_primes(n):
+    primes = []
+
+    sieve = get_sieve(n)
+    for i in xrange(2, n + 1):
+        if sieve[i]:
+            primes.append(i)
+
+    return primes
+
+
 print get_sieve(1)
 print get_sieve(2)
 print get_sieve(100)
+
+print get_primes(1)
+print get_primes(2)
+print get_primes(100)
