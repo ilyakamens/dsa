296a91a16d9b0def67a8dfa95c0f92f99478a402
Ilya Kamens <ilyakamens@gmail.com>
2014-09-08 16:36:19 -0700

    Adding reflector test cases

--- python/tests/all_tests.py
+++ python/tests/all_tests.py
@@ -4,6 +4,7 @@ __date__ = '9/8/14'
 
 from sieve_of_eratosthenes_test import *
 from prime_factorization_test import *
+from reflector_test import *
 
 class AllTestsTestCase(unittest.TestCase):
     """Test class to run all tests"""
@@ -13,6 +14,7 @@ class AllTestsTestCase(unittest.TestCase):
         suite = unittest.TestSuite()
         suite.addTests(unittest.TestLoader().loadTestsFromTestCase(SieveOfEratosthenesTestCase))
         suite.addTests(unittest.TestLoader().loadTestsFromTestCase(PrimeFactorizationTestCase))
+        suite.addTests(unittest.TestLoader().loadTestsFromTestCase(ReflectorTestCase))
 
 if __name__ == '__main__':
     unittest.main()
